e5d39dc00714e3311b049a59e1caca4adf6ba7b5
Sylvain MARION <s-marion@hotmail.fr>
2017-11-29 11:06:10 +0100

    Ajout de la gestion des états dans les activités stores & lumiere.

--- MaisonConnecte/app/src/main/java/com/a2017/ihm/m2gi/maisonconnecte/LumiereActivity.java
+++ MaisonConnecte/app/src/main/java/com/a2017/ihm/m2gi/maisonconnecte/LumiereActivity.java
@@ -2,12 +2,44 @@ package com.a2017.ihm.m2gi.maisonconnecte;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 
 public class LumiereActivity extends AppCompatActivity {
+
+    public static boolean isLumiereCouranteOn = false;
+    public static boolean isLumiereGeneraleOn = false;
+
+    private Switch switchLumiereCourante;
+    private Switch switchLumiereGenerale;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lumiere);
+
+        switchLumiereCourante = findViewById(R.id.switchLumiereCourante);
+        switchLumiereCourante.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                isLumiereCouranteOn = b;
+            }
+        });
+
+        switchLumiereGenerale = findViewById(R.id.switchLumiereGenerale);
+        switchLumiereGenerale.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                isLumiereGeneraleOn = b;
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        switchLumiereCourante.setChecked(isLumiereCouranteOn);
+        switchLumiereGenerale.setChecked(isLumiereGeneraleOn);
     }
 }
--- MaisonConnecte/app/src/main/java/com/a2017/ihm/m2gi/maisonconnecte/StoreActivity.java
+++ MaisonConnecte/app/src/main/java/com/a2017/ihm/m2gi/maisonconnecte/StoreActivity.java
@@ -2,12 +2,44 @@ package com.a2017.ihm.m2gi.maisonconnecte;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 
 public class StoreActivity extends AppCompatActivity {
+
+    public static boolean isStoreCourantOuvert = false;
+    public static boolean isStoreGeneralOuvert = false;
+
+    private Switch switchStoreCourant;
+    private Switch switchStoreGeneral;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_store);
+
+        switchStoreCourant = findViewById(R.id.switchStoreCourant);
+        switchStoreGeneral = findViewById(R.id.switchStoreGeneral);
+
+        switchStoreCourant.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                isStoreCourantOuvert = b;
+            }
+        });
+
+        switchStoreGeneral.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                isStoreGeneralOuvert = b;
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        switchStoreCourant.setChecked(isStoreCourantOuvert);
+        switchStoreGeneral.setChecked(isStoreGeneralOuvert);
     }
 }
